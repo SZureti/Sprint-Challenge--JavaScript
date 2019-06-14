2eb1204be287154c1161aeec77da4e6d2654d91d
SAIPHER ZURETI <saipherzureti@gmail.com>
2019-06-14 12:37:16 -0500

    Updated: classes, functions, object-arrays, and prototypes. Still working.

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -12,23 +12,54 @@
 
 // velociraptor, carnivorous, 15kg, 1.8m, Late Cretaceous
 
+let tyrannosaurus = {
+  "name": "Tyrannosaurus",
+  "diet": "carnivorous",
+  "weight": "7000kg",
+  "length": "12m",
+  "period": "Late Cretaceous"
+    // "speak": function(){
+    //   return "RAWERSRARARWERSARARARRRR!";
+    // }
+}
+
+tyrannosaurus.speak = function(){
+  return "RAWERSRARARWERSARARARRRR!";
+}
+
+let stegosaurus = {
+  "name": "Stegosaurus",
+  "diet": "herbivorous",
+  "weight": "2000kg",
+  "length": "9m",
+  "period": "Late Jurassic"
+}
+
+let velociraptor = {
+  "name": "Velociraptor",
+  "diet": "carnivorous",
+  "weight": "15kg",
+  "length": "1.8m",
+  "period": "Late Cretaceous"
+}
+
 // Using your dinosaur objects, log answers to these questions:
 
 // How much did tyrannosaurus weigh?
-console.log();
+console.log(tyrannosaurus['weight']);
 
 // What was the diet of a velociraptor?
-console.log();
+console.log(velociraptor['diet']);
 
 // How long was a stegosaurus?
-console.log();
+console.log(stegosaurus['length']);
 
 // What time period did tyrannosaurus live in?
-console.log();
+console.log(tyrannosaurus['period']);
 
 
 // Create a new roar method for the tyrannosaurus.  When called, return "RAWERSRARARWERSARARARRRR!" Log the result.
-console.log();
+console.log(tyrannosaurus.speak());
 
 
 // ==== Arrays ====
@@ -48,9 +79,18 @@ const graduates = [{"id":1,"first_name":"Cynde","university":"Missouri Southern 
 
 /* Request 1: Create a new array called universities that contains all the universities in the graduates array.  
 
+
 Once you have the new array created, sort the universities alphabetically and log the result. */
+// let universities = []
+// for (let i = 0; i < graduates.length; i++){
+//   universities.push(graduates[i]university);
+// }
+
 const universities = [];
-console.log(universities)
+for (let i = 0; i < graduates.length; i++){
+  universities.push(graduates[i].university);
+}
+console.log(universities.sort());
 
 /* Request 2: Create a new array called contactInfo that contains both first name and email of each student. 
 
@@ -59,12 +99,29 @@ Name email@example.com
 
 Log the result of your new array. */
 const contactInfo = [];
+graduates.forEach(function (firstName){
+  contactInfo.push(`${firstName.first_name} ${firstName.email}`);
+})
+
 console.log(contactInfo);
 
 
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called uni that contains them all. Log the result. */
-const uni = [];
-console.log(uni);
+// const uni = [];
+// for (let i = 0; i < graduates.length; i++){
+//   if(graduates[i].university.str.includes('uni'); {
+//     uni.push(graduates[i].university);
+//   }
+// }
+
+const uni = function(){
+  for (let i = 0; i < graduates.length; i++){
+  if(graduates[i].university.str.includes('uni')){
+    return uni.push(graduates[i].university);
+    }
+  }
+}
+console.log(uni());
 
 
 // ==== ADVANCED Array Methods ====
--- challenges/prototypes.js
+++ challenges/prototypes.js
@@ -5,6 +5,11 @@
 /* == Step 1: Base Constructor ==
   Create a constructor function named CuboidMaker that accepts properties for length, width, and height
 */
+function CuboidMaker(opts){
+    this.length = opts.length;
+    this.width = opts.width;
+    this.height = opts.height;
+  }
 
 
 /* == Step 2: Volume Method ==
@@ -12,6 +17,18 @@
   
   Formula for cuboid volume: length * width * height
 */
+function Volume(opts){
+  CuboidMaker.call(this, opts);
+  this.length = opts.length;
+  this.width = opts.width;
+  this.height = opts.height;
+}
+
+
+Volume.CuboidMaker.prototype = Object.create(CuboidMaker.prototype);
+Volume.CuboidMaker.prototype = function (){
+  return `${opts.length} * ${opts.width} * ${opts.height}`;
+}
 
 
 /* == Step 3: Surface Area Method ==
@@ -20,14 +37,40 @@
   Formula for cuboid surface area of a cube: 2 * (length * width + length * height + width * height)
 */
 
+function SurfaceArea(opts){
+  CuboidMaker.call(this, opts);
+  this.length = opts.length;
+  this.width = opts.width;
+  this.height = opts.height;
+}
+SurfaceArea.CuboidMaker.prototype = Object.create(CuboidMaker.prototype);
+SurfaceArea.CuboidMaker.prototype = function(){
+  return `2 * (${opts.length} * ${opts.width} + ${opts.length} * ${opts.height} + ${opts.width} * ${opts.height})`;
+}
 
 /* == Step 4: Create a new object that uses CuboidMaker ==
   Create a cuboid object that uses the new keyword to use our CuboidMaker constructor
   Add properties and values of length: 4, width: 5, and height: 5 to cuboid.   
 */
 
+// function Cuboid(newkey){
+//   CuboidMaker.call(this, newkey);
+//   this.length = newkey.length;
+//   this.width = newkey.width;
+//   this.height = newkey.height;
+// }
+
+// let Cuboid = {
+//   "length": 4,
+//   "width": 5,
+//   "height": 5
+// }
+
+// Cuboid.prototype = Object.create(CuboidMaker.prototype);
+
+
 // Test your volume and surfaceArea methods by uncommenting the logs below:
 // console.log(cuboid.volume()); // 100
-// console.log(cuboid.surfaceArea()); // 130
+console.log(cuboid.surfaceArea()); // 130
 
 
